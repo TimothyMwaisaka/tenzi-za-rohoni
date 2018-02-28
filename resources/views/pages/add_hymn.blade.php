e86a34a9e556543d284c92713b2863d91f106f52
Timothy Mwaisaka <cultimmy@gmail.com>
2018-03-01 02:03:13 +0300

    Added validation..

--- resources/views/pages/add_hymn.blade.php
+++ resources/views/pages/add_hymn.blade.php
@@ -71,28 +71,46 @@
                     {{ csrf_field() }}
                     <div class="box">
                         <div class="box-body">
-                                <div class="col-md-12">
-                                    <!-- text input -->
-                                    <div class="form-group col-md-2">
-                                        <label></label>
-                                        <input type="text" name="number" class="form-control"
-                                               placeholder="Song Number ...">
-                                    </div>
-                                    <div class="form-group col-md-10">
-                                        <label></label>
-                                        <input type="text" name="title" class="form-control"
-                                               placeholder="Song Tittle ...">
-                                    </div>
+                            <div class="col-md-12">
+                                <!-- text input -->
+                                <div class="form-group col-md-2 {{ $errors->has('number') ? ' has-error' : '' }}">
+                                    <label></label>
+                                    <input type="text" name="number" class="form-control"
+                                           placeholder="Song Number ...">
+                                    @if ($errors->has('number'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('number') }}</strong>
+                                    </span>
+                                    @endif
                                 </div>
+                                <div class="form-group col-md-10 {{ $errors->has('title') ? ' has-error' : '' }}">
+                                    <label></label>
+                                    <input type="text" name="title" class="form-control"
+                                           placeholder="Song Tittle ...">
+                                    @if ($errors->has('title'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('title') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
                         </div>
                     </div>
 
                     <div class="box">
                         <div class="box-body pad">
+                            <div class="form-group {{ $errors->has('title') ? ' has-error' : '' }}">
+                <textarea name="song" class="textarea" rows="20"
+                          placeholder="Song..."
+                          style="width: 100%; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">
 
-                <textarea name="song" class="textarea" rows="20" placeholder="Song..."
-                          style="width: 100%; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-
+                </textarea>
+                                @if ($errors->has('song'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('song') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
                         </div>
                     </div>
                     <input type="submit" name="submit" value="Save" class="btn btn-block btn-primary btn-flat">
