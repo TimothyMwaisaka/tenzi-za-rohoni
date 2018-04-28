eec083973bded112cb3df4da5f936b669267da40
Timothy Mwaisaka <cultimmy@gmail.com>
2018-04-28 23:07:01 +0300

    Updated route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'HymnController@index');
 Route::get('/add-songs', 'HymnController@create');
-Route::get('/view-songs/{id}' , ['as' => 'view-songs', 'uses' => 'HymnController@show']);
+Route::get('view-songs/{id}' , ['as' => 'view-songs', 'uses' => 'HymnController@show']);
 Route::post('/add-songs', 'HymnController@store');
 Route::get('/edit-songs/{id}', 'HymnController@edit');
 Route::post('/edit-songs/{id}', 'HymnController@update');
