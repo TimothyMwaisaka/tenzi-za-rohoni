92be001f378c11cca079821c1565971727ef02e3
Timothy Mwaisaka <cultimmy@gmail.com>
2018-05-02 01:26:16 +0300

    Added description meta tag

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="robots" content="index, follow" />
+    <meta name="description" content="Nyimbo za Tenzi za Rohoni">
     <title>{{ config('app.name') }} | {{ $title }}</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
