423c9c882a5bcaf3c50cfc6b1253c7637b59eaac
Timothy Mwaisaka <cultimmy@gmail.com>
2018-05-13 23:56:48 +0300

    Updated app name

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | framework needs to place the application's name in a notification or
     | any other location as required by the application or its packages.
     */
-    'name' => 'Tenzi',
+    'name' => 'Tenzi za Rohoni',
     //'name' => env('APP_NAME', 'Tenzi'),
 
     /*
